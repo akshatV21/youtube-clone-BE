a21b9250208dfe887f2d1acf63f25c647da37989
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-04 16:13:44 +0530

    completeing user model

--- src/api/models/user-model.js
+++ src/api/models/user-model.js
@@ -1,4 +1,4 @@
-const { Schema } = require("mongoose")
+const { Schema, Types } = require("mongoose")
 
 const userSchema = new Schema({
   username: { type: String, required: true },
@@ -7,6 +7,23 @@ const userSchema = new Schema({
   settings: {
     playerSize: { type: String },
     volume: { type: Number },
-    
   },
+  interests: { type: [String] },
+  watchLater: { type: [Types.ObjectId], default: [], ref: "videos" },
+  playlists: {
+    type: [
+      {
+        playlistName: String,
+        vidoes: [Types.ObjectId],
+        currentlyOn: {
+          videoIndex: { type: Number },
+          timestamp: { type: Number },
+        },
+      },
+    ],
+    default: [],
+    ref: "videos",
+  },
+  isCreator: { type: Boolean, default: false },
+  channel: { type: Types.ObjectId, ref: "channels" },
 })
