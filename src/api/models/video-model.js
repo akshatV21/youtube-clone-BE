99e430abf8577fdf8484fad84f9e147f3aa37974
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-05 10:42:09 +0530

    added videos model

---
+++ src/api/models/video-model.js
@@ -0,0 +1,19 @@
+const { Schema, Types, model } = require("mongoose")
+
+const videoSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  link: { type: String, required: true },
+  likes: { type: Number },
+  dislikes: { type: Number },
+  comments: { type: [Types.ObjectId], default: [], ref: "comments" },
+  views: { type: Number, default: 0 },
+  increasedSubCount: { type: Number, default: 0 },
+  private: { type: Boolean, default: false },
+  thumbnail: { type: String, required: true },
+  
+})
+
+const VideoModel = model("video", videoSchema)
+
+module.exports = VideoModel
