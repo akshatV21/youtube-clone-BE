0ea0234e79379a648473baf7f24fc123066cb246
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-05 14:10:41 +0530

    added channels

--- src/api/middlewares/video-middlewares.js
+++ src/api/middlewares/video-middlewares.js
@@ -3,6 +3,10 @@ const NodeError = require("../../error/error-object")
 
 const validateVideoPostRequest = async (req, res, next) => {
   try {
+    const user = req.user
+
+    if (!user.channel) throw new NodeError("403", "You must have a channel!")
+
     const { title, description, link, thumbnail, category } = req.body
     if (!title) throw new NodeError(400, CONTENTS.NULL_TITLE)
     if (!description) throw new NodeError(400, CONTENTS.NULL_DESCRIPTION)
@@ -10,7 +14,7 @@ const validateVideoPostRequest = async (req, res, next) => {
     if (!thumbnail) throw new NodeError(400, CONTENTS.NULL_THUMBNAIL)
     if (!category) throw new NodeError(400, CONTENTS.NULL_CATEGORY)
 
-    req.video = { title, description, thumbnail, category, link }
+    req.video = { channelId: user.channel._id, title, description, thumbnail, category, link }
     next()
   } catch (error) {
     next(error)
--- src/api/models/video-model.js
+++ src/api/models/video-model.js
@@ -1,6 +1,7 @@
 const { Schema, Types, model } = require("mongoose")
 
 const videoSchema = new Schema({
+  channelId: { type: Types.ObjectId, required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   link: { type: String, required: true },
