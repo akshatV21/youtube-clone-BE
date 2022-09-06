de7e6a74efac8cadc93ddae609235b7c26e79ab5
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-06 10:10:47 +0530

    added channels model

---
+++ src/api/models/channel-model.js
@@ -0,0 +1,15 @@
+const { Schema, Types, model } = require("mongoose")
+
+const channelSchema = new Schema({
+  userId: { type: Types.ObjectId, required: true },
+  name: { type: String, required: true },
+  subscribers: { type: [Types.ObjectId], default: [], ref: "users" },
+  totalViews: { type: Number, default: 0 },
+  totalLikes: { type: Number, default: 0 },
+  videos: { type: [Types.ObjectId], default: [], ref: "videos" },
+  playlists: { type: [Types.ObjectId], default: [], ref: "playlists" },
+})
+
+const ChannelModel = model("channel", channelSchema)
+
+module.exports = ChannelModel
