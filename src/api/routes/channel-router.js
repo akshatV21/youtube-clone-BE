03e3bd75fe143ce4a9b2aceb6994bb985bf19e0e
akshatV21 <a.vishwakarma2110@gmail.com>
2022-09-07 14:13:28 +0530

    added get channel videos controller

--- src/api/routes/channel-router.js
+++ src/api/routes/channel-router.js
@@ -1,5 +1,12 @@
 const { Router } = require("express")
-const { httpGetChannel, httpCreateChannel, httpSubscribeChannel, httpUnsubscribeChannel, httpCreateChannelPlaylist } = require("../controllers/channel-controller")
+const {
+  httpGetChannel,
+  httpCreateChannel,
+  httpSubscribeChannel,
+  httpUnsubscribeChannel,
+  httpCreateChannelPlaylist,
+  httpGetChannelVideos,
+} = require("../controllers/channel-controller")
 const { authorizeUser } = require("../middlewares/auth-middlewares")
 const { validateChannelRequest } = require("../middlewares/channel-middlewares")
 const { validatePlaylistRequest } = require("../middlewares/playlist-middlewares")
@@ -10,7 +17,7 @@ channelRouter.post("/create", authorizeUser, httpCreateChannel)
 channelRouter.post("/playlist", authorizeUser, validateChannelRequest, validatePlaylistRequest, httpCreateChannelPlaylist)
 
 channelRouter.get("/", authorizeUser, validateChannelRequest, httpGetChannel)
-channelRouter.get("/videos", authorizeUser)
+channelRouter.get("/videos", authorizeUser, httpGetChannelVideos)
 
 channelRouter.patch("/subscribe", authorizeUser, validateChannelRequest, httpSubscribeChannel)
 channelRouter.patch("/unsubscribe", authorizeUser, validateChannelRequest, httpUnsubscribeChannel)
